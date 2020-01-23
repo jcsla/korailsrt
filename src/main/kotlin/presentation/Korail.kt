428e277e26c42ceee8f7204a5bece5c5857a3fa9
phillip <phillip.leejy@gmail.com>
2020-01-23 20:12:50 +0900

    encapsulation of korail api.

--- src/main/kotlin/presentation/Korail.kt
+++ src/main/kotlin/presentation/Korail.kt
@@ -1,6 +1,13 @@
 package presentation
 
 import data.api.KorailApi
+import entity.dto.request.LoginRequest
+import entity.dto.request.ScheduleViewRequest
+import entity.dto.request.TicketReservationRequest
+import entity.dto.response.LoginResponse
+import entity.dto.response.ScheduleViewResponse
+import entity.dto.response.TicketReservationResponse
+import entity.dto.response.Train
 import presentation.di.AppComponent
 import presentation.di.DaggerAppComponent
 
@@ -11,7 +18,19 @@ object Korail {
         appComponent
     }
 
-    val api: KorailApi by lazy {
+    private val api: KorailApi by lazy {
         appComponent.korailApi()
+    }
+
+    fun login(id: String, pwd: String): LoginResponse? {
+        return api.login(LoginRequest(id, pwd).getRequestBody()).execute().body()
+    }
+
+    fun searchTrain(date: String, time: String, trainType: String, adultCount: String): ScheduleViewResponse? {
+        return api.searchTrain(ScheduleViewRequest(date, time, trainType, adultCount).getRequestBody()).execute().body()
+    }
+
+    fun reserveTicket(train: Train): TicketReservationResponse? {
+        return api.reserveTicket(TicketReservationRequest(train).getRequestBody()).execute().body()
     }
 }
