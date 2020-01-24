c922cf8ae360ff566f58bfc7b46ee37db9910a16
phillip <phillip.leejy@gmail.com>
2020-01-24 17:02:58 +0900

    refactoring

--- src/main/kotlin/presentation/Korail.kt
+++ src/main/kotlin/presentation/Korail.kt
@@ -1,6 +1,5 @@
 package presentation
 
-import data.api.KorailApi
 import entity.dto.request.LoginRequest
 import entity.dto.request.ScheduleViewRequest
 import entity.dto.request.TicketReservationRequest
@@ -8,29 +7,28 @@ import entity.dto.response.LoginResponse
 import entity.dto.response.ScheduleViewResponse
 import entity.dto.response.TicketReservationResponse
 import entity.dto.response.Train
-import presentation.di.AppComponent
 import presentation.di.DaggerAppComponent
 
 object Korail {
 
-    private val appComponent: AppComponent by lazy {
+    private val appComponent = lazy {
         val appComponent = DaggerAppComponent.builder().build()
         appComponent
     }
 
-    private val api: KorailApi by lazy {
-        appComponent.korailApi()
+    private val api =  lazy {
+        appComponent.value.korailApi()
     }
 
     fun login(id: String, pwd: String): LoginResponse? {
-        return api.login(LoginRequest(id, pwd).getRequestBody()).execute().body()
+        return api.value.login(LoginRequest(id, pwd).getRequestBody()).execute().body()
     }
 
     fun searchTrain(date: String, time: String, trainType: String, adultCount: String): ScheduleViewResponse? {
-        return api.searchTrain(ScheduleViewRequest(date, time, trainType, adultCount).getRequestBody()).execute().body()
+        return api.value.searchTrain(ScheduleViewRequest(date, time, trainType, adultCount).getRequestBody()).execute().body()
     }
 
     fun reserveTicket(train: Train): TicketReservationResponse? {
-        return api.reserveTicket(TicketReservationRequest(train).getRequestBody()).execute().body()
+        return api.value.reserveTicket(TicketReservationRequest(train).getRequestBody()).execute().body()
     }
 }
